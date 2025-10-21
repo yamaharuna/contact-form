55df6e0412ebae51b314fa2c91727a3063392f68
yamaharuna <harunack17@icloud.com>
2025-10-21 12:08:28 +0900

    ヘッダー

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: "#1A9DD9", 
+        color: "white",
+        padding: "16px 0",
+        textAlign: "center",
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+      }}
+    >
+      お問い合わせフォーム
+    </header>
+  );
+}
